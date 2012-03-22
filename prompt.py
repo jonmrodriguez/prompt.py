2cc8ce5e921f4c918aaf63bc3f53251d2bdd0b8a
Jon Rodriguez <luciddreamworld@gmail.com>
2012-03-22 01:21:56 -0700

    progressing forward. new question is how to simplify tput.py in a way where it knows its old state from environ, and ideally even exports those vars upward. otherwise, gonna have to store the state in a file, with a process-unique tempfilename marked for this purpose by bashrc

--- prompt.py
+++ prompt.py
@@ -1,7 +1,49 @@
 #! /usr/bin/python2.6
 
-print "hello 2.6"
 
 """
 prompt.py is to completely replace $PS1 and $PROMPT_COMMAND
 """
+
+import sys
+from os import environ
+sys.path.append(environ['PY_LIBS_DIR'])
+import subprocess26 as subprocess
+import tput # TODO make tput know the state of the colors by using the env vars FG_COLOR and BG_COLOR
+
+# CONSTANTS
+
+N_COLORS = 8
+DEFAULT_FG_COLOR = 7
+# default bg_color is SHLVL, to distinguish away terminals
+
+# DETERMINE bg_color
+
+bg_color = int(environ['SHLVL']) # fall-back default
+
+if 'BG_COLOR' in environ:
+    bg_color = int(environ['BG_COLOR'])
+
+bg_color = bg_color % N_COLORS
+
+
+# DETERMINE fg_color
+
+fg_color = DEFAULT_FG_COLOR
+
+if 'FG_COLOR' in environ:
+    fg_color = int(environ['FG_COLOR'])
+
+fg_color = fg_color % N_COLORS
+
+
+# DETERMINE fg_color_of_prompt (itself)
+
+
+
+
+
+# STRINGS THAT CONSIST THE PROMPT TEXT
+
+print bg_color, fg_color
+
